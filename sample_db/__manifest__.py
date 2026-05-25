37f7c8516f7ebdf4a57bdd7103ee2be418ec7925
Priyansh Khatri <priyanshkhatri65@gmail.com>
2026-05-25 14:54:24 +0530

    Create sample odoo module

---
+++ sample_db/__manifest__.py
@@ -0,0 +1,35 @@
+{
+    'name': 'Sample Custom Module',
+
+    'version': '1.0.0',
+
+    'category': 'Custom',
+
+    'summary': 'Learning custom module',
+
+    'description': """
+Sample Custom Module
+====================
+
+Learning custom module development in Odoo.
+""",
+
+    'author': 'Priyansh Khatri',
+
+    'license': 'LGPL-3',
+
+    'depends': [
+        'base',
+    ],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/sample_module_views.xml',
+    ],
+
+    'installable': True,
+
+    'application': True,
+
+    'auto_install': False,
+}
---
+++ sample_db/models/sample_model.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+
+class SampleModel(models.Model):
+    _name = 'sample.model'
+    _description = 'Sample Model'
+
+    name = fields.Char(string="Name", required=True)
+    age = fields.Integer(string="Age")
+    email = fields.Char(string="Email")
+    active = fields.Boolean(string="Active", default=True)
