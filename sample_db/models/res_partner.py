3e932782d62964f3ad33de1b98fc2964e76623d1
Priyansh Khatri <priyanshkhatri65@gmail.com>
2026-05-28 12:15:01 +0530

    res_partner file change

--- sample_db/models/res_partner.py
+++ sample_db/models/res_partner.py
@@ -1,8 +1,8 @@
 from odoo import models, fields
 
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     customer_code = fields.Char(string="Customer Code")
-    age = fields.Integer(string="Age")
-    city = fields.Char(string="City")
+    loyalty_points = fields.Integer(string="Loyalty Points")
