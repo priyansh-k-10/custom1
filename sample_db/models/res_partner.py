8f2b1444b861b41a078d9d80e0bb2d65244fbae4
Priyansh Khatri <priyanshkhatri65@gmail.com>
2026-05-29 12:36:26 +0530

    added computed logic

--- sample_db/models/res_partner.py
+++ sample_db/models/res_partner.py
@@ -1,8 +1,22 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     customer_code = fields.Char(string="Customer Code")
+
     loyalty_points = fields.Integer(string="Loyalty Points")
+
+    loyalty_status = fields.Char(
+        string="Loyalty Status",
+        compute="_compute_loyalty_status"
+    )
+
+    @api.depends('loyalty_points')
+    def _compute_loyalty_status(self):
+        for rec in self:
+            if rec.loyalty_points >= 100:
+                rec.loyalty_status = "Gold"
+            else:
+                rec.loyalty_status = "Silver"
